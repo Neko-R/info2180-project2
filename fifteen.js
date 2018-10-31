498843a10da82b1e393060d0f3f8c92f6a981d76
Niikou <reidneko@gmail.com>
2018-10-30 23:27:44 -0500

    Shuffle one piece works.

--- fifteen.js
+++ fifteen.js
@@ -1,7 +1,7 @@
 //INFO2180 - Web Development I
 //Project 2 - The Fifteen Puzzles
 //ID - 620100200
-var empty = {'row':3, 'col':3};
+var empty = {'row': 3, 'col': 3};
 
 window.onload = function() {
     var puzArea = document.getElementById("puzzlearea");
@@ -12,36 +12,38 @@ window.onload = function() {
     for (var i = 0; i < pieces.length; i++) { 
         pieces[i].classList.add("puzzlepiece"); 
         pieces[i].id = String(i+1); //give each puzzle piece a unique id
-        $("#"+String(i)).css({"left":String((i*100)%400)+"px", "top":String(Math.floor(i/4)*100)+"px", "background-position": "-"+String((i*100)%400)+"px " + String((Math.floor(i/4)*300)%400)+"px"})
+        $("#"+String(i+1)).css({"left":String((i*100)%400)+"px", "top":String(Math.floor(i/4)*100)+"px", "background-position": "-"+String((i*100)%400)+"px " + String((Math.floor(i/4)*300)%400)+"px"})
     };
 
     $("div.puzzlepiece").hover(enter,leave); //adding event handler to add "movablepiece" to piece's classlist when hovered over and remove class when leave you piece 
     $("div.puzzlepiece").click(pressed); //adding event handler for when piece is clicked, it'll move if it can
 
     shuffle.onclick = () => { //shuffle pieces
-        let index = []; //creating array of indexes for pieces
-        for(var j = 0; j<=pieces.length;j++)
-            index.push(j);
 
+        let neighbours = [];
         for (var k = 0; k < pieces.length; k++) { 
-            i = index[Math.floor(Math.random()*index.length)]; //get random index
-
-            index = index.filter(function(value, index, arr){ //remove index obtained
-                return value != i;
-            });
-            //placing puzzle piece of index k in position i on board.
-            pieces[k].setAttribute("style", "left: "+String((i*100)%400)+"px; " + "top: "+String(Math.floor(i/4)*100)+"px;" + "background-position: -"+String((k*100)%400)+"px " + String((Math.floor(k/4)*300)%400)+"px;");
+            let p = $("#"+String(k+1)).position();
+            let r = Math.ceil(p.top)/100;
+            let c =  Math.ceil(p.left)/100;
+            let pb = neighbour(r,c);
+            if(pb[0]){
+                neighbours.push({"iD": (k+1), "row":r, "col": c, "pos":pb[1]});
+            }
         }
-    }
-
+        
+        piece = neighbours[Math.floor(Math.random()*neighbours.length)]; //get random piece that can move
+        move(piece.iD, piece.pos, piece.row, piece.col); //move piece obtained
+        }
+   
 }
 
 function enter(){ //hover piece
     let p = $("#"+String(this.id)).position();
-    if( neighbour( Math.ceil(p.top)/100, Math.ceil(p.left)/100)[0] ){
+    let r = Math.ceil(p.top)/100;
+    let c =  Math.ceil(p.left)/100;
+    if( neighbour(r,c)[0]){
         this.classList.add("movablepiece");
     }
-
 }
 
 function leave(){ //leave piece
@@ -49,27 +51,26 @@ function leave(){ //leave piece
 }
 
 function pressed(){ //piece pressed
-    let id = this.id;
-    let p = $("#"+String(id)).position();
+    let p = $("#"+String(this.id)).position();
     let r = Math.ceil(p.top)/100;
     let c =  Math.ceil(p.left)/100;
-    move(r,c,id); //move piece if can move
+    let pb = neighbour(r,c);
+    if(pb[0]){
+        move(this.id, pb[1], r, c); //move piece if can move
+    }
 }
 
-function move(r,c,id){ //move piece if it's a neighbour of blank square
-    let pb = neighbour(r,c);
-    if( pb[0] ){
-        if(pb[1] == "top"){ //if top piece
-            $("#"+String(id)).css("top",String((r+1)*100)+"px"); //move down one row 
-        }else if(pb[1] == "bottom"){
-            $("#"+String(id)).css("top",String((r-1)*100)+"px"); //move up one row
-        }else if(pb[1] == "left"){
-            $("#"+String(id)).css("left", +String((c+1)*100)+"px"); //move right one column
-        }else if(pb[1] == "right"){
-            $("#"+String(id)).css("left",String((c-1)*100)+"px"); //move left one column
-        }
-        empty = {"row": r, "col": c}; //empty coordinates now equals the previous position of piece just pressed
+function move(id,pos,r,c){ //move piece if it's a neighbour of blank square
+    if(pos == "top"){ //if top piece
+        $("#"+String(id)).css("top",String((r+1)*100)+"px"); //move down one row 
+    }else if(pos == "bottom"){
+        $("#"+String(id)).css("top",String((r-1)*100)+"px"); //move up one row
+    }else if(pos == "left"){
+        $("#"+String(id)).css("left", +String((c+1)*100)+"px"); //move right one column
+    }else if(pos == "right"){
+        $("#"+String(id)).css("left",String((c-1)*100)+"px"); //move left one column
     }
+    empty = {"row": r, "col": c}; //empty coordinates now equals the previous position of piece just pressed
 }
 
 function neighbour(r,c){ //check if the row-column of piece is a neighbour of blank square
