ad95db7675a4516ba1c49221c9e2347bcad68289
Niikou <reidneko@gmail.com>
2018-10-31 10:00:46 -0500

    Completed Projected.

--- fifteen.js
+++ fifteen.js
@@ -1,6 +1,7 @@
 //INFO2180 - Web Development I
 //Project 2 - The Fifteen Puzzles
 //ID - 620100200
+//Note: I completed the 'Animations and/or transitions' feature
 
 var empty = {'row': 3, 'col': 3}; //keep track of the position of the empty square
 var puzArea; //full puzzle area
@@ -49,40 +50,24 @@ function pressed(){ //piece pressed
         let c =  Math.ceil(p.left)/100; //value of left attribute (0-3)
         let pb = neighbour(r,c);
         if(pb[0]){
-        move(this.id, pb[1], r, c,true); //move piece if can move
+            animateMove(this.id, pb[1], r, c); //move piece if can move
         }
     }
 }
 
-function move(id,pos,r,c,a){ //move piece if it's a neighbour of blank square
+function animateMove(id,pos,r,c){ //move piece if it's a neighbour of blank square
     let coord;
     if(pos == "top"){ //if top piece
-        if(a){
-            aniMoveFromTop(id,r*100,(r+1)*100); //move down one row 
-        }else{
-            $("#"+id).css("top", String((r+1)*100)+"px"); 
-        }
+        aniMoveFromTop(id,r*100,(r+1)*100); //move down one row 
         coord = String(r+1)+String(c);
     }else if(pos == "bottom"){
-        if(a){
-            aniMoveFromBttm(id,r*100,(r-1)*100); //move up one row 
-        }else{
-            $("#"+id).css("top", String((r-1)*100)+"px"); 
-        }
+        aniMoveFromBttm(id,r*100,(r-1)*100); //move up one row 
         coord = String(r-1)+String(c);
     }else if(pos == "left"){
-        if(a){
-            aniMoveFromLeft(id,c*100,(c+1)*100); //move up one row 
-        }else{
-            $("#"+id).css("left", String((c+1)*100)+"px"); 
-        }
+        aniMoveFromLeft(id,c*100,(c+1)*100); //move up one row 
         coord = String(r)+String(c+1);
     }else if(pos == "right"){
-        if(a){
-            aniMoveFromRight(id,c*100,(c-1)*100); //move up one row 
-        }else{
-            $("#"+id).css("top", String((c-1)*100)+"px"); 
-        }
+        aniMoveFromRight(id,c*100,(c-1)*100); //move up one row 
         coord = String(r)+String(c-1);
     }
     if(id == coord){ //if current position of piece is the same as its id (which indicates the correct position it should be in)
@@ -92,6 +77,35 @@ function move(id,pos,r,c,a){ //move piece if it's a neighbour of blank square
         testWin[id] = false; //set testWin to false since piece is not in correct position
     }
     empty = {"row": r, "col": c}; //empty coordinates now equals the previous position of piece just pressed
+}
+
+function shuffleMove(id,pos,r,c){ //move piece if it's a neighbour of blank square
+    let coord;
+    if(pos == "top"){ //if top piece
+        $("#"+id).css("top", String((r+1)*100)+"px"); 
+        coord = String(r+1)+String(c);
+    }else if(pos == "bottom"){
+        $("#"+id).css("top", String((r-1)*100)+"px"); 
+        coord = String(r-1)+String(c);
+    }else if(pos == "left"){
+        $("#"+id).css("left", String((c+1)*100)+"px"); 
+        coord = String(r)+String(c+1);
+    }else if(pos == "right"){
+        $("#"+id).css("left", String((c-1)*100)+"px"); 
+        coord = String(r)+String(c-1);
+    }
+    if(id == coord){ //if current position of piece is the same as its id (which indicates the correct position it should be in)
+        undoMove(id); //Undo the move because there is a possibility that shuffle will rearrange the board correctly.
+    }
+    empty = {"row": r, "col": c}; //empty coordinates now equals the previous position of piece just pressed
+}
+
+function undoMove(id){
+    let p = $("#"+id).position(); //get the value of the piece's 'top' and 'left' attribute
+    let r = Math.ceil(p.top)/100; //vaue of top attribute (0-3)
+    let c =  Math.ceil(p.left)/100; //value of left attribute (0-3)
+    let pb = neighbour(r,c);
+    shuffleMove(id, pb[1], r, c); //move from current spot
 }
 
 function neighbour(r,c){ //check if the row-column of piece is a neighbour of blank square
@@ -110,7 +124,7 @@ function neighbour(r,c){ //check if the row-column of piece is a neighbour of bl
 
 function shufflePieces(){ //shuffle pieces
     win = false; //set win to false for new game since board is now shuffled
-    for(var j = 0; j<50; j++){
+    for(var j = 0; j<100; j++){
         let neighbours = []; //array to hold neighbours of empty square
         for (var k = 0; k < pieces.length; k++) { 
             let p = $("#"+pieces[k].id).position(); //get the value of the piece's 'top' and 'left' attribute
@@ -121,9 +135,8 @@ function shufflePieces(){ //shuffle pieces
                 neighbours.push({"iD": pieces[k].id, "row":r, "col": c, "pos":pb[1]}); //add neighbour to array
             }
         }
-        
         piece = neighbours[Math.floor(Math.random()*neighbours.length)]; //get random piece that can move
-        move(piece.iD, piece.pos, piece.row, piece.col,false); //move piece obtained
+        shuffleMove(piece.iD, piece.pos, piece.row, piece.col); //move piece obtained
     }
 }
 
@@ -138,54 +151,54 @@ function checkWin(){ //check is pieces are in correct order/position
     }
 }
 
-function aniMoveFromTop(id,cur,fut){
-    var pos = cur;
-    var t = setInterval(frame, 5);
+function aniMoveFromTop(id,cur,fut){ //animate piece move from top
+    var pos = cur; //state current position to move from
+    var t = setInterval(frame, 5); //set interval/timer
     function frame() {
         if (pos == fut) {
             clearInterval(t);
         } else {
-            pos++; 
-            $("#"+id).css("top",String(pos)+"px"); 
+            pos++;
+            $("#"+id).css("top",String(pos)+"px"); //incrementally change top position value of piece over time
         }
     }
 }
 
-function aniMoveFromBttm(id,cur,fut){
-    var pos = cur;
-    var t = setInterval(frame, 5);
+function aniMoveFromBttm(id,cur,fut){ //animate piece move from bottom
+    var pos = cur; //state current position to move from
+    var t = setInterval(frame, 5); //set interval/timer
     function frame() {
         if (pos == fut) {
             clearInterval(t);
         } else {
             pos--; 
-            $("#"+id).css("top",String(pos)+"px"); 
+            $("#"+id).css("top",String(pos)+"px"); //decrementally change top position value of piece over time
         }
     }
 }
 
-function aniMoveFromLeft(id,cur,fut){
-    var pos = cur;
-    var t = setInterval(frame, 5);
+function aniMoveFromLeft(id,cur,fut){ //animate piece move from left
+    var pos = cur; //state current position to move from
+    var t = setInterval(frame, 5); //set interval/timer
     function frame() {
         if (pos == fut) {
             clearInterval(t);
         } else {
             pos++; 
-            $("#"+id).css("left",String(pos)+"px"); 
+            $("#"+id).css("left",String(pos)+"px"); //incrementally change left position value of piece over time
         }
     }
 }
 
-function aniMoveFromRight(id,cur,fut){
-    var pos = cur;
-    var t = setInterval(frame, 5);
+function aniMoveFromRight(id,cur,fut){ //animate piece move from right
+    var pos = cur; //state current position to move from
+    var t = setInterval(frame, 5); //set interval/timer
     function frame() {
         if (pos == fut) {
             clearInterval(t);
         } else {
             pos--; 
-            $("#"+id).css("left",String(pos)+"px"); 
+            $("#"+id).css("left",String(pos)+"px"); //decrementally change top position value of piece over time
         }
     }
 }
